7b1480fe5610d94a52546d08c082410711488469
czz <2851411231@qq.com>
2019-05-04 17:11:58 +0800

    媒资管理程序

---
+++ xc-service-manage-media/src/main/java/com/xuecheng/manage_media/service/MediaUploadService.java
@@ -0,0 +1,201 @@
+package com.xuecheng.manage_media.service;
+
+import com.xuecheng.framework.domain.media.MediaFile;
+import com.xuecheng.framework.domain.media.response.CheckChunkResult;
+import com.xuecheng.framework.domain.media.response.MediaCode;
+import com.xuecheng.framework.exception.ExceptionCast;
+import com.xuecheng.framework.model.response.CommonCode;
+import com.xuecheng.framework.model.response.ResponseResult;
+import com.xuecheng.manage_media.dao.MediaFileRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+
+@Service
+public class MediaUploadService {
+    private final static Logger LOGGER = LoggerFactory.getLogger(MediaUploadService.class);
+
+    @Autowired
+    MediaFileRepository eediaFileRepository;
+
+    @Value("${xc-service-manage-media.upload-location}")
+    String uploadPath;
+
+    /**
+     *  根据文件md5得到文件路径
+     * 规则：
+     * 一级目录：md5的第一个字符
+     * 二级目录：md5的第二个字符
+     * 三级目录：md5
+     * 文件名：md5+文件扩展名
+     *
+     * @param fileMd5 文件md5值
+     * @param fileExt 文件扩展名
+     * @return
+     */
+    private String getFilePath(String fileMd5, String fileExt) {
+        String filePath = uploadPath + fileMd5.substring(0, 1) + "/" + fileMd5.substring(1, 2) +
+                "/" + fileMd5 + "/" + fileMd5 + "." + fileExt;
+        return filePath;
+    }
+
+    /**
+     * 得到文件目录相对路径，路径中去掉根目录
+     *
+     * @param fileMd5
+     * @param fileExt
+     * @return
+     */
+    private String getFileFoldRelativePath(String fileMd5, String fileExt) {
+        String filePath = fileMd5.substring(0, 1) + "/" + fileMd5.substring(1, 2) + "/" + fileMd5 + "/";
+        return filePath;
+    }
+
+    /**
+     * 得到文件所在目录
+     *
+     * @param fileMd5
+     * @return
+     */
+    private String getFileFolderPath(String fileMd5) {
+        String fileFolderPath = uploadPath + fileMd5.substring(0, 1) + "/" + fileMd5.substring(1, 2) + "/" + fileMd5 + "/";
+        return fileFolderPath;
+    }
+
+    /**
+     * 创建文件目录
+     *
+     * @param fileMd5
+     * @return
+     */
+    private boolean createFileFold(String fileMd5) {
+        // 创建上传文件目录
+        String fileFolderPath = getFileFolderPath(fileMd5);
+        File fileFolder = new File(fileFolderPath);
+        if (!fileFolder.exists()) {
+            // 创建文件夹
+            boolean mkdirs = fileFolder.mkdirs();
+            return mkdirs;
+        }
+        return true;
+    }
+
+    /**
+     * 文件上传注册
+     *
+     * @param fileMd5
+     * @param fileName
+     * @param fileSize
+     * @param mimetype
+     * @param fileExt
+     * @return
+     */
+    public ResponseResult register(String fileMd5, String fileName, String fileSize, String mimetype, String fileExt) {
+        // 检查文件是否上传
+        // 1.得到文件的路径
+        String filePath = getFilePath(fileMd5, fileExt);
+        File file = new File(fileExt);
+        // 2.查询数据库文件是否存在
+        Optional<MediaFile> optional = eediaFileRepository.findById(fileMd5);
+        // 文件存在直接返回
+        if (file.exists() && optional.isPresent()) {
+            ExceptionCast.cast(MediaCode.UPLOAD_FILE_REGISTER_EXIST);
+        }
+        boolean fileFold = createFileFold(fileMd5);
+        if (!fileFold) {
+            ExceptionCast.cast(MediaCode.UPLOAD_FILE_REGISTER_CREATEFOLDER_FAIL);
+        }
+        return new ResponseResult(CommonCode.SUCCESS);
+    }
+
+    /**
+     * 得到块文件所在目录
+     *
+     * @param fileMd5
+     * @return
+     */
+    private String getChunkFileFolderPath(String fileMd5) {
+        String fileChunkFolderPath = getFileFolderPath(fileMd5) + "/" + "chunks" + "/";
+        return fileChunkFolderPath;
+    }
+
+    public CheckChunkResult checkChunk(String fileMd5, String chunk, String chunkSize) {
+        // 得到块文件所在路径
+        String chunkFileFolderPath = getChunkFileFolderPath(fileMd5);
+        // 块文件的名称以1，2，3...序号命名，没有扩展名
+        File chunkFile = new File(chunkFileFolderPath);
+        if (chunkFile.exists()) {
+            return new CheckChunkResult(MediaCode.CHUNK_FILE_EXIST_CHECK, true);
+        } else {
+            return new CheckChunkResult(MediaCode.CHUNK_FILE_EXIST_CHECK, false);
+        }
+    }
+
+    /**
+     * 创建块文件目录
+     *
+     * @param fileMd5
+     * @return
+     */
+    private boolean creatChunkFileFolder(String fileMd5) {
+        String chunkFileFolderPath = getChunkFileFolderPath(fileMd5);
+        File file = new File(chunkFileFolderPath);
+        if (!file.exists()) {
+            // 创建块文件目录
+            boolean mkdirs = file.mkdirs();
+            return mkdirs;
+        }
+        return true;
+
+    }
+
+    /**
+     * 上传分块
+     *
+     * @param file
+     * @param fileMd5
+     * @param chunk
+     * @return
+     */
+    public ResponseResult uploadchunk(MultipartFile file, String fileMd5, String chunk) {
+        if (file == null) {
+            ExceptionCast.cast(MediaCode.UPLOAD_FILE_REGISTER_ISNULL);
+        }
+        // 创建块文件目录
+        boolean fileFold = creatChunkFileFolder(fileMd5);
+        // 块文件
+        File chunkFile = new File(getChunkFileFolderPath(fileMd5) + chunk);
+        // 上传块文件
+        InputStream inputStream = null;
+        FileOutputStream outputStream = null;
+        try {
+            inputStream = file.getInputStream();
+            outputStream = new FileOutputStream(chunkFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.error("upload chunk file fail:{}", e.getMessage());
+            ExceptionCast.cast(MediaCode.CHUNK_FILE_UPLOAD_FAIL);
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return new ResponseResult(CommonCode.SUCCESS);
+    }
+}
